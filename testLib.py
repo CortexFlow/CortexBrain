71356955e9e9a95b3ad49b4aed7b3cc784fc7846
Lorenzo Tettamanti <lorenzo.tettamanti5@gmail.com>
2024-08-22 20:48:27 +0200

    fix issues 2.1

--- testLib.py
+++ testLib.py
@@ -1,9 +1,15 @@
 def CheckLib(libraries):
     not_installed = []
 
+    special_imports = {
+        'PyQtWebEngine': 'PyQt5.QtWebEngineWidgets',  # Nome del modulo effettivo per importare PyQtWebEngine
+    }
+
     for library in libraries:
+        # Usa un nome di importazione speciale se presente
+        module_to_import = special_imports.get(library, library)
         try:
-            __import__(library)
+            __import__(module_to_import)
             print(f"The library '{library}' is installed.")
         except ImportError:
             print(f"The library '{library}' is NOT installed.")
@@ -17,9 +23,18 @@ def CheckLib(libraries):
         print("\nAll libraries are installed.")
 
 
+if __name__ == "__main__":
+    # Example usage:
+    libraries_to_check = ['numpy', 'pandas', 'matplotlib', 'prophet', 'requests', 'asyncio',
+                          'aiohttp', 'networkx', 'folium', 'pydot',
+                          'geopy', 'simpy', 'pymongo', 'snowflake', 'PyQt5', 'PyQtWebEngine', 'osmnx', 'mesa']
+    CheckLib(libraries_to_check)
+
+
+
 if __name__=="__main__":
     # Example usage:
     libraries_to_check = ['numpy', 'pandas', 'matplotlib', 'prophet', 'requests', 'asyncio',
                         'aiohttp', 'networkx', 'folium', 'pydot',
-                        'geopy', 'simpy','pymongo','snowflake','pyqt5','PyQtWebEngine','osmnx','mesa']
+                        'geopy', 'simpy','pymongo','snowflake','PyQt5','PyQtWebEngine','osmnx','mesa']
     CheckLib(libraries_to_check)
