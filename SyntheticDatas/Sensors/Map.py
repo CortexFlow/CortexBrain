3557149561b2e4b3a14586bf1bbdf812ab37ee83
Lorenzo Tettamanti <lorenzo.tettamanti5@gmail.com>
2024-08-31 21:14:53 +0200

    fixed some typos and some code issues

--- SyntheticDatas/Sensors/Map.py
+++ SyntheticDatas/Sensors/Map.py
@@ -1,9 +1,12 @@
 import numpy as np
 import networkx as nx
 import folium
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 from BaseSensor import Sensor  # Make sure this import is correct
-
+from utils.DataProcessor import DataProcessor
 
 class Map:
     """Class responsible for creating and managing the map with sensors."""
@@ -47,3 +50,40 @@ class Map:
         # Save the map to an HTML file or display it in a Jupyter notebook
         map.save('sensor_map.html')
         print("Map has been saved to 'sensor_map.html'.")
+
+
+class IndoorArea:
+    def __init__(self):
+        self.sensors = []
+
+    def addSensorIndoor(self, sensor):
+        self.sensors.append(sensor)
+
+    def create_map_with_sensors(self):
+        folium_map = folium.Map(location=[45.81, 8.98], zoom_start=14,
+                                tiles='https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', attr='Google Maps')
+
+        for sensor in self.sensors:
+            folium.Marker(
+                location=[sensor.lat, sensor.lon],
+                popup=f"""
+                <strong>{sensor.label}</strong><br>
+                Angle: {sensor.angle}°<br>
+                FOV: {sensor.fov}°<br>
+                Range: {sensor.range}
+                """,
+                icon=folium.Icon(color='blue', icon='cloud')
+            ).add_to(folium_map)
+
+            points = DataProcessor.generateCameraPoints(sensor.lat, sensor.lon, sensor.angle, sensor.fov, sensor.range)
+            if points:
+                folium.Polygon(
+                    locations=points,
+                    color='green',
+                    fill=True,
+                    fill_color='green',
+                    fill_opacity=0.4
+                ).add_to(folium_map)
+
+        folium_map.save("sensor_map.html")
+        print("Folium map with sensors and fields of view has been saved to 'sensor_map.html'.")
