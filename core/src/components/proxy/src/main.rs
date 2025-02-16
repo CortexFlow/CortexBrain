d7b27205fdf1a6cd3e32799e739739124f0eface
LorenzoTettamanti <lorenzo.tettamanti5@gmail.com>
2025-02-16 11:44:05 +0100

    [#62]: added cortexflow proxy module (not tested yet). updated configs

--- core/src/components/proxy/src/main.rs
+++ core/src/components/proxy/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+//TODO: basic proxy functionalities
+//TODO: add integration with prometheus loggin system
+//TODO: add load balancer between dns servers
+
+
+mod proxy;
+mod vars;
+
+use kube::{api::Api,Client};
+use k8s_openapi::api::core::v1::ConfigMap;
+use shared::{apiconfig::EdgeProxyConfig, default_api_config::ConfigType};
+use proxy::Proxy;
+
+
+#[tokio::main]
+async fn main()-> Result<(),anyhow::Error> {
+
+    let client = Client::try_default().await?;
+    let configmap: Api<ConfigMap> = Api::namespaced(client.clone(), "default");
+
+    let proxycfg= EdgeProxyConfig::load_from_configmap(configmap,ConfigType::Default).await?;
+    let proxy = Proxy::new(proxycfg,client.clone()).await?;
+
+    proxy.start().await;
+    proxy.get_info().await;
+
+    Ok(())
 }
---
+++ core/src/components/proxy/src/vars.rs
@@ -0,0 +1,25 @@
+use lazy_static::lazy_static;
+use prometheus::{register_histogram_vec, register_int_counter_vec, HistogramVec, IntCounterVec};
+
+
+lazy_static!{
+    /* One of the main benefits of using lazy_static is the ability to store thread-safe global variables. 
+    Because lazy static values are initialized in a thread-safe manner, they can be safely accessed 
+    from multiple threads without the need for additional synchronization. 
+    This can be especially useful in cases where you want to avoid the overhead of locking and unlocking shared resources. 
+
+    lazy static documentation: https://blog.logrocket.com/rust-lazy-static-pattern/#how-lazy-static-works
+    */
+
+    pub static ref DNS_REQUEST: IntCounterVec = register_int_counter_vec!(
+        "total_dns_requests",
+        "Total DNS Requests",
+        &["client-ip"]
+    ).unwrap();
+
+    pub  static ref DNS_RESPONSE_TIME: HistogramVec = register_histogram_vec!(
+        "dns response time",
+        "DNS response time",
+        &["server"]
+    ).unwrap();
+}
