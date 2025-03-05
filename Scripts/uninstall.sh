a645003be31fb60f8cbb6c0fa41d2ab350afea4d
LorenzoTettamanti <lorenzo.tettamanti5@gmail.com>
2025-03-05 15:40:46 +0100

    [#69]: updated uninstall script.sh

--- Scripts/uninstall.sh
+++ Scripts/uninstall.sh
@@ -8,3 +8,6 @@ kubectl delete svc proxy-service -n cortexflow
 echo "deployment and services deleted"
 kubectl get deployment -n cortexflow
 kubectl get svc -n cortexflow
+
+echo "deleting cortexflow namespace"
+kubectl delete namespace cortexflow
