bde1de63334a704bd930e632d733833628a2dbdf
LorenzoTettamanti <lorenzotettamanti5@gmail.com>
2025-04-27 16:29:32 +0200

    [#82]: added xdp main.rs (comments)

--- core/src/components/xdp/src/main.rs
+++ core/src/components/xdp/src/main.rs
@@ -1,6 +1,7 @@
 /* contains the code for the kernel xdp manipulation. this code lives in
-the kernel space only and needs to be attached to a program in the user space
+the kernel space only and needs to be attached to a "main" program that lives in the user space
 */
+
 #![no_std] //no standard library
 #![no_main] //no main entrypoint
 
@@ -8,21 +9,19 @@ use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
 use aya_log_ebpf::info;
 
 #[xdp]
-//simple hello world to test
 pub fn xdp_hello(ctx: XdpContext) -> u32 {
-    match unsafe { init_xdp(ctx) } {
+    match unsafe { init_xdp(&ctx) } {
         Ok(ret) => ret,
         Err(_) => xdp_action::XDP_ABORTED,
     }
 }
 
-//xdp init
-unsafe fn init_xdp(ctx: XdpContext) -> Result<u32, u32> {
-    info!(&ctx, "Received a packet");
+unsafe fn init_xdp(ctx: &XdpContext) -> Result<u32, u32> {
+    info!(ctx, "Received a packet");
     Ok(xdp_action::XDP_PASS)
 }
 
-#[panic_handler] //panic handler in case of fatal errors
+#[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
