2b12eb6049fc7ce104c5ed51957202aab5a69e08
Lorenzo Tettamanti <lorenzo.tettamanti5@gmail.com>
2025-05-24 13:41:36 +0200

    [#82]: updated map,rs comments

--- core/src/components/maps/src/map.rs
+++ core/src/components/maps/src/map.rs
@@ -1,71 +1,75 @@
 /*
-    Init a bpf map to save the user space pod data
-    to use in the kernel space and user space
+ * Init a bpf map to save the user space pod data
+ * to use in the kernel space and user space
 */
 #![no_std]
 #![no_main]
 
-use bytemuck::{Pod,Zeroable};
-use aya_ebpf::{macros::map, maps::HashMap as KernelSpaceMap}; //aya_ebpf is the kernel space libary
 use aya_ebpf::maps::PerfEventArray;
-/* unsafe impl Zeroable for SVCKey {} //implemente zeroable 
+use aya_ebpf::{macros::map, maps::HashMap as KernelSpaceMap}; //aya_ebpf is the kernel space libary
+use bytemuck::{Pod, Zeroable};
+
+/* unsafe impl Zeroable for SVCKey {} //implemente zeroable
 unsafe impl Zeroable for SVCValue {} */
 
 #[repr(C)]
 /*
-    match the C fields alignment. tells the compiler that the rappresentation
-    must follow the C rules. disable the rust compiler realignment
-
-    In this case Rust struct and C are unitary equivalent
-
+ * match the C fields alignment. tells the compiler that the rappresentation
+ * must follow the C rules. disable the rust compiler realignment
+ *
+ *In this case Rust struct and C are unitary equivalent
 */
-#[derive(Clone, Copy, Pod, Zeroable,Debug)]
+#[derive(Clone, Copy, Pod, Zeroable, Debug)]
 pub struct SVCKey {
     pub service_name: [u8; 64],
 }
 
 #[repr(C)] //match the C fields alignment
-#[derive(Clone, Copy, Pod, Zeroable,Debug)]
+#[derive(Clone, Copy, Pod, Zeroable, Debug)]
 pub struct SVCValue {
     pub ip: [u8; 4],
     pub port: u32,
 }
 
 #[repr(C)]
-#[derive(Clone,Debug,Pod,Zeroable,Copy)]
-pub struct BackendPorts{
-    pub ports: [u16;4],
-    pub index: usize
+#[derive(Clone, Debug, Pod, Zeroable, Copy)]
+pub struct BackendPorts {
+    pub ports: [u16; 4],
+    pub index: usize,
 }
 
 //enable Pod (Plain of data) data type
 /* unsafe impl Pod for SVCKey {}
 unsafe impl Pod for SVCValue {} */
 /*
-
-    Doc:
-    POD (Plain Old Data) types are marked with the trait, indicating that they can be
-    duplicated simply by copying their memory representation.
-
-    This trait allows the Rust compiler to efficiently handle data creating bit-for-bit copies
-    without invoking user-defined methods
-    POD types do not involve pointers or complex data structures, they are easier to manage in terms of
-    memory allocation and deallocation
+    * Doc:
+    * POD (Plain Old Data) types are marked with the trait, indicating that they can be
+    * duplicated simply by copying their memory representation.
+    *
+    * This trait allows the Rust compiler to efficiently handle data creating bit-for-bit copies
+    * without invoking user-defined methods
+    * POD types do not involve pointers or complex data structures, they are easier to manage in terms of
+    * memory allocation and deallocation
 
 */
 
+/*
+ * Maps
+*/
 
-
-/* Maps  */
-
-
-#[map(name = "services")] //connect the map name "SERVICES" to the HasMap in the BPF bytecode
-//init a BPF_MAP_HASH_TYPE to store the resolved service values
+#[map(name = "services")]
+/*
+ * connect the map name "SERVICES" to the HasMap in the BPF bytecode
+ * init a BPF_MAP_HASH_TYPE to store the resolved service values
+*/
 pub static mut SERVICES: KernelSpaceMap<SVCKey, SVCValue> =
     KernelSpaceMap::with_max_entries(1024, 0);
 
-#[map(name = "Backend_ports")] //connect the map name "BACKEND_PORTS" to the HasMap in the BPF bytecode
-//init a BPF_MAP_HASH_TYPE to store the resolved service values
+#[map(name = "Backend_ports")]
+/*
+ * connect the map name "BACKEND_PORTS" to the HasMap in the BPF bytecode
+ * init a BPF_MAP_HASH_TYPE to store the resolved service values
+*/
 pub static mut BACKEND_PORTS: KernelSpaceMap<u16, BackendPorts> =
     KernelSpaceMap::with_max_entries(10, 0);
 
@@ -80,17 +84,17 @@ pub fn str_to_u8_64(s: &str) -> [u8; 64] {
     buf
 }
 pub fn u32_to_u8_4(s: u32) -> [u8; 4] {
+    //32 bit ---> 4 bytes
     let mut buf = [0u8; 4];
-    // Un u32 è 4 byte
-    let bytes = s.to_le_bytes();  // Questo produce [u8; 4]
-    buf[..4].copy_from_slice(&bytes);  // Copia solo i primi 4 byte
+    let bytes = s.to_le_bytes(); // this produce [u8,4]
+    buf[..4].copy_from_slice(&bytes); // Only copy the first 4 bytes
     buf
 }
 
 pub fn u32_to_u8_64(s: u32) -> [u8; 64] {
+    //32 bit ---> 4 bytes
     let mut buf = [0u8; 64];
-    // Un u32 è 4 byte
-    let bytes = s.to_le_bytes();  // Questo produce [u8; 4]
-    buf[..4].copy_from_slice(&bytes);  // Copia solo i primi 4 byte
+    let bytes = s.to_le_bytes(); //this produce [u8,4]
+    buf[..4].copy_from_slice(&bytes); // Only copy the first 4 bytes
     buf
 }
