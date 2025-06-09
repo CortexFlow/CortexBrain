e5ec1a2f8b0cf2940f2f96fa60d457cccf3bf8de
LorenzoTettamanti <lorenzotettamanti5@gmail.com>
2025-06-09 14:29:32 +0200

    [#94]: added cli install,update,help command structure

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,23 +1,51 @@
 mod essential;
 mod install;
 
-use clap::{Arg, Parser, Subcommand};
+use clap::{ Error, Parser, Subcommand };
+use clap::command;
 
-#[derive(Parser)]
-#[command(author="CortexFlow",version="0.1",about=None,long_about=None)]
+use crate::essential::{ info, update_cli };
+use crate::install::install_cortexflow;
+
+#[derive(Parser, Debug)]
+#[command(author = "CortexFlow", version = "0.1", about = None, long_about = None)]
 struct Args {
-    name: String,
+    //name: String,
+    #[clap(subcommand)]
+    cmd: Option<Commands>,
 }
+
+#[derive(Subcommand, Debug, Clone)]
 enum Commands {
-    /* continue from here */
-    Install(String),
-    Update(String),
+    /* list of available commands */
+    Install,
+    Update,
+    Info,
 }
-fn args_parser() {
+
+fn args_parser() -> Result<(), Error> {
     let args = Args::parse();
-    println!("Arguments {:?}", args.name);
+    println!("Arguments {:?}", args.cmd);
+    match args.cmd {
+        Some(Commands::Install) => {
+            install_cortexflow();
+            Ok(())
+        }
+        Some(Commands::Update) => {
+            update_cli();
+            Ok(())
+        }
+        Some(Commands::Info) => {
+            info();
+            Ok(())
+        }
+        None => {
+            eprintln!("CLI unknown argument. Cli arguments passed: {:?}", args.cmd);
+            Ok(())
+        }
+    }
 }
 
 fn main() {
-    args_parser();
+    let _ = args_parser();
 }
