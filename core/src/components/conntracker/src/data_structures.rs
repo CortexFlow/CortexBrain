12806f91633af7b080204eb1312121a649d53af4
LorenzoTettamanti <lorenzo.tettamanti5@gmail.com>
2025-07-24 00:27:44 +0200

    [#119]: improved code readibility in core/conntracker. Removed unused code,libraries

---
+++ core/src/components/conntracker/src/data_structures.rs
@@ -0,0 +1,58 @@
+use aya_ebpf::{macros::map, maps::{LruPerCpuHashMap, PerfEventArray}};
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct PacketLog {
+    pub proto: u8,
+    pub src_ip: u32,
+    pub src_port: u16,
+    pub dst_ip: u32,
+    pub dst_port: u16,
+    pub pid: u64,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct NetnsLog {
+    pub netns: u32,
+    pub pid: u64,
+}
+
+// This structure is only for active connections
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct ConnArray {
+    pub src_ip: u32,
+    pub dst_ip: u32,
+    pub src_port: u16,
+    pub dst_port: u16,
+    pub proto: u8,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+pub struct VethLog {
+    pub name: [u8; 16],
+    pub state: u64, //state var type: long unsigned int
+    pub dev_addr: [u32; 8],
+    pub event_type: u8, //i choose 1 for veth creation or 2 for veth destruction
+    //pub netns: u32,
+    //pub ns_common_ptr : usize,
+}
+
+#[map(name = "EventsMap")]
+pub static mut EVENTS: PerfEventArray<PacketLog> = PerfEventArray::new(0);
+
+#[map(name = "NetnsMap")]
+pub static mut NET_EVENTS: PerfEventArray<NetnsLog> = PerfEventArray::new(0);
+//TODO: ConnectionMap needs a rework after implementing issue #105
+#[map(name = "ConnectionMap")]
+pub static mut ACTIVE_CONNECTIONS: LruPerCpuHashMap<u16, ConnArray> =
+    LruPerCpuHashMap::with_max_entries(65536, 0);
+
+#[map(name = "ConnectionTrackerMap")]
+pub static mut CONNTRACKER: LruPerCpuHashMap<ConnArray, u8> =
+    LruPerCpuHashMap::with_max_entries(65536, 0);
+
+#[map(name = "veth_identity_map")]
+pub static mut VETH_EVENTS: PerfEventArray<VethLog> = PerfEventArray::new(0);
