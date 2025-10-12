dc1c6191d514fbf45342c236ee8e00b55759ce93
LorenzoTettamanti <lorenzotettamanti5@gmail.com>
2025-10-12 21:05:21 +0200

    [#105]: updated agent api: added AddIpToBlocklist and CheckBlocklist grpc endpoints in agent.proto . Added add_ip_to_blocklist and check_blocklist functions in api.rs module. Updated requests module: added send_create_blocklist_request and send_check_blocklist_request

--- core/api/src/mod.rs
+++ core/api/src/mod.rs
@@ -1,2 +1,3 @@
 pub mod api;
 pub mod agent;
+pub mod requests;
--- core/api/src/requests.rs
+++ core/api/src/requests.rs
@@ -1,17 +1,21 @@
 use anyhow::Error;
 use std::result::Result::Ok;
-use tonic::{Request, Response, Streaming, transport::Channel};
+use tonic::{ Request, Response, Streaming, transport::Channel };
 use tonic_reflection::pb::v1::{
-    ServerReflectionRequest, ServerReflectionResponse,
-    server_reflection_client::ServerReflectionClient, server_reflection_request::MessageRequest,
+    ServerReflectionRequest,
+    ServerReflectionResponse,
+    server_reflection_client::ServerReflectionClient,
+    server_reflection_request::MessageRequest,
 };
 
 use crate::agent::agent_client::AgentClient;
 use crate::agent::ActiveConnectionResponse;
 use crate::agent::RequestActiveConnections;
+use crate::agent::BlocklistResponse;
+use crate::agent::AddIpToBlocklistRequest;
 
 pub async fn send_active_connection_request(
-    mut client: AgentClient<Channel>,
+    mut client: AgentClient<Channel>
 ) -> Result<Response<ActiveConnectionResponse>, Error> {
     let request = Request::new(RequestActiveConnections { pod_ip: None });
     let response = client.active_connections(request).await?;
@@ -19,17 +23,31 @@ pub async fn send_active_connection_request(
 }
 
 pub async fn get_all_features(
-    mut client: ServerReflectionClient<Channel>,
+    mut client: ServerReflectionClient<Channel>
 ) -> Result<Response<Streaming<ServerReflectionResponse>>, Error> {
     let request = ServerReflectionRequest {
         host: "".to_string(),
-        message_request: Some(MessageRequest::FileContainingSymbol(
-            "agent.Agent".to_string(),
-        )),
+        message_request: Some(MessageRequest::FileContainingSymbol("agent.Agent".to_string())),
     };
-    let response = client
-        .server_reflection_info(tokio_stream::iter(vec![request]))
-        .await?;
+    let response = client.server_reflection_info(tokio_stream::iter(vec![request])).await?;
 
+    Ok(response)
+}
+
+pub async fn send_create_blocklist_request(
+    mut client: AgentClient<Channel>,
+    ip: &str
+) -> Result<Response<BlocklistResponse>, Error> {
+    let ip = Some(ip.to_string());
+    let request = Request::new(AddIpToBlocklistRequest { ip });
+    let response = client.add_ip_to_blocklist(request).await?;
+    Ok(response)
+}
+
+pub async fn send_check_blocklist_request(
+    mut client: AgentClient<Channel>
+) -> Result<Response<BlocklistResponse>, Error> {
+    let request = Request::new(());
+    let response = client.check_blocklist(request).await?;
     Ok(response)
 }
