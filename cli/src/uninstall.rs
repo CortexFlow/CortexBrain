9682dc3ac37f3d7286f7a6d2ffa0de168747813e
LorenzoTettamanti <lorenzotettamanti5@gmail.com>
2025-11-06 22:24:53 +0100

    [Code refactoring]: refactored uninstall commands. Added better error handling, Added documentation to every function

--- cli/src/uninstall.rs
+++ cli/src/uninstall.rs
@@ -1,127 +1,167 @@
-use crate::essential::{Environments, get_config_directory, read_configs};
+use crate::{ essential::read_configs, status };
 use colored::Colorize;
-use std::io::stdin;
-use std::process::Command;
+use std::{ io::stdin, process::Command, time::Duration, thread };
 use tracing::debug;
 
-use std::thread;
-use std::time::Duration;
+use crate::essential::{ BASE_COMMAND, InstallationError, connect_to_client };
+use kube::{ Error, core::ErrorResponse };
 
-pub fn uninstall() {
-    //let file_path = get_config_directory().unwrap().1;
-    //let dir_config_path = get_config_directory().unwrap().0;
-    //debug!("file_path variable:{:?}", dir_config_path);
-    //let env_from_file = read_configs(file_path.clone());
-    //let user_env = Environments::try_from(env_from_file.to_lowercase());
+//docs:
+//
+// This function manages the uninstall process for the cortexflow components
+// Steps:
+//      - connects to kubernetes client
+//      - display the uninstall options
+//      - read the user input (e.g. 1 > all components)
+//      - uninstall the selected component or the whole namespace
+//
+// Returns an InstallationError if something fails
 
-    //match user_env {
-    //    Ok(cluster_environment) => {
-    let env = "kubectl".to_string();
-    println!(
-        "{} {}",
-        "=====>".blue().bold(),
-        "Uninstalling cortexflow..."
-    );
-    let mut userinput: String = String::new();
-    println!("{} {}", "=====>".blue().bold(), "Select one option:");
-    display_uninstall_options();
-    stdin()
-        .read_line(&mut userinput)
-        .expect("Error reading user input");
+pub async fn uninstall() -> Result<(), InstallationError> {
+    match connect_to_client().await {
+        Ok(_) => {
+            println!("{} {}", "=====>".blue().bold(), "Uninstalling cortexflow...");
+            let mut userinput: String = String::new();
+            println!("{} {}", "=====>".blue().bold(), "Select one option:");
+            display_uninstall_options();
+            stdin().read_line(&mut userinput).expect("Error reading user input");
 
-    let trimmed_input = userinput.trim();
-    if trimmed_input == "1" {
-        uninstall_all(&env);
-        println!(
-            "{} {}",
-            "=====>".blue().bold(),
-            "Do you want to remove the command line metadata? [y/n]"
-        );
-
-        //clear the user input before assigning a new value
-        userinput.clear();
-        stdin()
-            .read_line(&mut userinput)
-            .expect("Error reading user input");
-
-        if userinput.trim() == "y" {
-            println!(
-                "{} {}",
-                "=====>".blue().bold(),
-                "Deleting metadata config files"
-            );
-            //println!(
-            //    "{} {}: {:?}",
-            //    "=====>".blue().bold(),
-            //    "Removing",
-            //    dir_config_path.clone()
-            //);
-            //rm_dir(dir_config_path.as_os_str().to_str().unwrap());
-        } else if userinput.trim() == "n" {
-            println!(
-                "{} {}",
-                "=====>".blue().bold(),
-                "Skipping metadata config files deletion"
-            );
+            let trimmed_input = userinput.trim();
+            if trimmed_input == "1" {
+                uninstall_all().await?;
+            } else if trimmed_input == "2" {
+                uninstall_component("deployment", "cortexflow-identity").await?;
+            }
+            Ok(())
         }
-    } else if trimmed_input == "2" {
-        uninstall_component("deployment", "cortexflow-identity", &env.to_owned());
+        Err(_) => {
+            Err(
+                InstallationError::ClientError(
+                    Error::Api(ErrorResponse {
+                        status: "failed".to_string(),
+                        message: "Failed to connect to kubernetes client".to_string(),
+                        reason: "Your cluster is probably disconnected".to_string(),
+                        code: 404,
+                    })
+                )
+            )
+        }
     }
 }
-// Err(e) => println!("An error occured while reading the config files: {}", e),
-//}
-//}
+
+//docs:
+//
+// This function only print the uninstall options
 
 fn display_uninstall_options() {
     println!("{} {}", "=====>".blue().bold(), "1 > all");
     println!("{} {}", "=====>".blue().bold(), "2 > identity-service");
 }
 
-fn uninstall_all(env: &str) {
-    println!(
-        "{} {}",
-        "=====>".blue().bold(),
-        "Deleting cortexflow components".red()
-    );
-    //uninstall_component("namespace", "cortexflow", env);
-    let output = Command::new(env)
-        .args(["delete", "namespace", "cortexflow"])
-        .output()
-        .expect("Error deleting cortexflow namespace");
+//docs:
+//
+// This function manages the uninstall of the whole cortexflow namespace
+// Steps:
+//      - connects to kubernetes client
+//      - execute the command to uninstall the cortexflow namespace
+//
+// Returns an InstallationError if something fails
 
-    if !output.status.success() {
-        eprintln!(
-            "Error deleting cortexflow namespace:\n{}",
-            String::from_utf8_lossy(&output.stderr)
-        );
-    } else {
-        println!("✅ Removed cortexflow namespace");
+async fn uninstall_all() -> Result<(), InstallationError> {
+    match connect_to_client().await {
+        Ok(_) => {
+            println!("{} {}", "=====>".blue().bold(), "Deleting cortexflow components".red());
+            let output = Command::new(BASE_COMMAND)
+                .args(["delete", "namespace", "cortexflow"])
+                .output()
+                .map_err(|e| InstallationError::InstallerError {
+                    reason: format!("Failed to execute delete command: {}", e),
+                })?;
+
+            if output.status.success() {
+                println!("✅ Removed cortexflow namespace");
+                Ok(())
+            } else {
+                let stderr = String::from_utf8_lossy(&output.stderr);
+                eprintln!("Error deleting cortexflow namespace. Error: {} ", stderr);
+                Err(InstallationError::InstallerError {
+                    reason: format!("Failed to delete cortexflow namespace. Error: {}", stderr),
+                })
+            }
+        }
+        Err(_) => {
+            Err(
+                InstallationError::ClientError(
+                    Error::Api(ErrorResponse {
+                        status: "failed".to_string(),
+                        message: "Failed to connect to kubernetes client".to_string(),
+                        reason: "Your cluster is probably disconnected".to_string(),
+                        code: 404,
+                    })
+                )
+            )
+        }
     }
 }
 
-fn uninstall_component(component_type: &str, component: &str, env: &str) {
-    println!(
-        "{} {} {}",
-        "=====>".blue().bold(),
-        "Deleting service",
-        component
-    );
-    let output = Command::new(env)
-        .args(["delete", component_type, component, "-n", "cortexflow"])
-        .output()
-        .expect("Error deleting cortexflow-identity");
+//docs:
+//
+// This function manages the uninstall of given cortexflow components
+// Steps:
+//      - connects to kubernetes client
+//      - executes the command to uninstall a given component
+//
+// Returns an InstallerError if something fails
 
-    if !output.status.success() {
-        eprintln!(
-            "Error deleting: {}:\n{}",
-            component,
-            String::from_utf8_lossy(&output.stderr)
-        );
-    } else {
-        println!("✅ Removed component {}", component);
+async fn uninstall_component(
+    component_type: &str,
+    component: &str
+) -> Result<(), InstallationError> {
+    match connect_to_client().await {
+        Ok(_) => {
+            println!("{} {} {}", "=====>".blue().bold(), "Deleting service", component);
+
+            let output = Command::new(BASE_COMMAND)
+                .args(["delete", component_type, component, "-n", "cortexflow"])
+                .output()
+                .map_err(|e| InstallationError::InstallerError {
+                    reason: format!("Failed to execute delete command: {}", e),
+                })?;
+
+            if output.status.success() {
+                println!("✅ Removed component {}", component);
+                Ok(())
+            } else {
+                let stderr = String::from_utf8_lossy(&output.stderr);
+                eprintln!("Error deleting {}:\n{}", component, stderr);
+                Err(InstallationError::InstallerError {
+                    reason: format!("Failed to delete component '{}': {}", component, stderr),
+                })
+            }
+        }
+        Err(_) => {
+            Err(
+                InstallationError::ClientError(
+                    Error::Api(ErrorResponse {
+                        status: "failed".to_string(),
+                        message: "Failed to connect to kubernetes client".to_string(),
+                        reason: "Your cluster is probably disconnected".to_string(),
+                        code: 404,
+                    })
+                )
+            )
+        }
     }
 }
 
+//
+//
+//docs:
+//
+// This function is deprecated and will be removed in the next version
+//
+// Do not include or refactor this function
+#[deprecated(since = "0.1.4")]
 fn rm_dir(directory_to_remove: &str) {
     let output = Command::new("rm")
         .args(["-rf", directory_to_remove])
