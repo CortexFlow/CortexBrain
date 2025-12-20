c3618c8fecd4572c30ecf64ce1367524cacfb2cd
LorenzoTettamanti <lorenzo.tettamanti5@gmail.com>
2025-12-20 11:34:01 +0100

    [#150]:added unit tests. Updated extract_pod_uid function logic

--- core/src/components/identity/src/helpers.rs
+++ core/src/components/identity/src/helpers.rs
@@ -1,29 +1,37 @@
 #![allow(warnings)]
 use crate::enums::IpProtocols;
-use crate::structs::{ PacketLog, VethLog, TcpPacketRegistry };
+use crate::structs::{PacketLog, TcpPacketRegistry, VethLog};
 use anyhow::Error;
 use aya::programs::tc::SchedClassifierLinkId;
 use aya::{
     Bpf,
-    maps::{ MapData, perf::PerfEventArrayBuffer },
-    programs::{ SchedClassifier, TcAttachType },
+    maps::{MapData, perf::PerfEventArrayBuffer},
+    programs::{SchedClassifier, TcAttachType},
 };
 use bytes::BytesMut;
+use cortexbrain_common::constants;
+use k8s_openapi::api::core::v1::Pod;
+use kube::api::ObjectList;
+use kube::{Api, Client};
 use nix::net::if_::if_nameindex;
-use tokio::time;
-use tracing_subscriber::fmt::format;
 use std::collections::HashMap;
 use std::fs;
 use std::hash::Hash;
+use std::path::PathBuf;
 use std::result::Result::Ok;
 use std::sync::Mutex;
-use std::path::PathBuf;
 use std::time::Duration;
-use std::{ borrow::BorrowMut, net::Ipv4Addr, sync::{ Arc, atomic::{ AtomicBool, Ordering } } };
-use tracing::{ error, info, warn };
-use kube::{ Api, Client };
-use k8s_openapi::api::core::v1::Pod;
-use cortexbrain_common::constants;
+use std::{
+    borrow::BorrowMut,
+    net::Ipv4Addr,
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
+};
+use tokio::time;
+use tracing::{debug, error, info, warn};
+use tracing_subscriber::fmt::format;
 
 /*
  * TryFrom Trait implementation for IpProtocols enum
@@ -47,7 +55,7 @@ impl TryFrom<u8> for IpProtocols {
 pub async fn display_events<T: BorrowMut<MapData>>(
     mut perf_buffers: Vec<PerfEventArrayBuffer<T>>,
     running: Arc<AtomicBool>,
-    mut buffers: Vec<BytesMut>
+    mut buffers: Vec<BytesMut>,
 ) {
     while running.load(Ordering::SeqCst) {
         for buf in perf_buffers.iter_mut() {
@@ -56,9 +64,8 @@ pub async fn display_events<T: BorrowMut<MapData>>(
                     for i in 0..events.read {
                         let data = &buffers[i];
                         if data.len() >= std::mem::size_of::<PacketLog>() {
-                            let pl: PacketLog = unsafe {
-                                std::ptr::read(data.as_ptr() as *const _)
-                            };
+                            let pl: PacketLog =
+                                unsafe { std::ptr::read(data.as_ptr() as *const _) };
                             let src = reverse_be_addr(pl.src_ip);
                             let dst = reverse_be_addr(pl.dst_ip);
                             let src_port = u16::from_be(pl.src_port);
@@ -69,19 +76,13 @@ pub async fn display_events<T: BorrowMut<MapData>>(
                                 std::result::Result::Ok(proto) => {
                                     info!(
                                         "Event Id: {} Protocol: {:?} SRC: {}:{} -> DST: {}:{}",
-                                        event_id,
-                                        proto,
-                                        src,
-                                        src_port,
-                                        dst,
-                                        dst_port
+                                        event_id, proto, src, src_port, dst, dst_port
                                     );
                                 }
                                 Err(_) => {
                                     info!(
                                         "Event Id: {} Protocol: Unknown ({})",
-                                        event_id,
-                                        pl.proto
+                                        event_id, pl.proto
                                     );
                                 }
                             };
@@ -111,7 +112,7 @@ pub async fn display_veth_events<T: BorrowMut<MapData>>(
     mut perf_buffers: Vec<PerfEventArrayBuffer<T>>,
     running: Arc<AtomicBool>,
     mut buffers: Vec<BytesMut>,
-    mut link_ids: Arc<Mutex<HashMap<String, SchedClassifierLinkId>>>
+    mut link_ids: Arc<Mutex<HashMap<String, SchedClassifierLinkId>>>,
 ) {
     while running.load(Ordering::SeqCst) {
         for buf in perf_buffers.iter_mut() {
@@ -120,9 +121,8 @@ pub async fn display_veth_events<T: BorrowMut<MapData>>(
                     for i in 0..events.read {
                         let data = &buffers[i];
                         if data.len() >= std::mem::size_of::<VethLog>() {
-                            let vethlog: VethLog = unsafe {
-                                std::ptr::read(data.as_ptr() as *const _)
-                            };
+                            let vethlog: VethLog =
+                                unsafe { std::ptr::read(data.as_ptr() as *const _) };
 
                             let name_bytes = vethlog.name;
 
@@ -152,19 +152,21 @@ pub async fn display_veth_events<T: BorrowMut<MapData>>(
                                         state,
                                         dev_addr
                                     );
-                                    match
-                                        attach_detach_veth(
-                                            bpf.clone(),
-                                            vethlog.event_type,
-                                            veth_name,
-                                            link_ids.clone()
-                                        ).await
+                                    match attach_detach_veth(
+                                        bpf.clone(),
+                                        vethlog.event_type,
+                                        veth_name,
+                                        link_ids.clone(),
+                                    )
+                                    .await
                                     {
                                         std::result::Result::Ok(_) => {
                                             info!("Attach/Detach veth function attached correctly");
                                         }
-                                        Err(e) =>
-                                            error!("Error attaching Attach/Detach function. Error : {}", e),
+                                        Err(e) => error!(
+                                            "Error attaching Attach/Detach function. Error : {}",
+                                            e
+                                        ),
                                     }
                                 }
                                 Err(_) => info!("Unknown name or corrupted field"),
@@ -211,9 +213,12 @@ async fn attach_detach_veth(
     bpf: Arc<Mutex<Bpf>>,
     event_type: u8,
     iface: &str,
-    link_ids: Arc<Mutex<HashMap<String, SchedClassifierLinkId>>>
+    link_ids: Arc<Mutex<HashMap<String, SchedClassifierLinkId>>>,
 ) -> Result<(), anyhow::Error> {
-    info!("attach_detach_veth called: event_type={}, iface={}", event_type, iface);
+    info!(
+        "attach_detach_veth called: event_type={}, iface={}",
+        event_type, iface
+    );
     match event_type {
         1 => {
             let mut bpf = bpf.lock().unwrap();
@@ -232,7 +237,10 @@ async fn attach_detach_veth(
             let mut link_ids = link_ids.lock().unwrap();
             match program.attach(iface, TcAttachType::Ingress) {
                 std::result::Result::Ok(link_id) => {
-                    info!("Program 'identity_classifier' attached to interface {}", iface);
+                    info!(
+                        "Program 'identity_classifier' attached to interface {}",
+                        iface
+                    );
                     link_ids.insert(iface.to_string(), link_id);
                 }
                 Err(e) => error!("Error attaching program to interface {}: {:?}", iface, e),
@@ -263,7 +271,7 @@ async fn attach_detach_veth(
 pub async fn display_tcp_registry_events<T: BorrowMut<MapData>>(
     mut perf_buffers: Vec<PerfEventArrayBuffer<T>>,
     running: Arc<AtomicBool>,
-    mut buffers: Vec<BytesMut>
+    mut buffers: Vec<BytesMut>,
 ) {
     while running.load(Ordering::SeqCst) {
         for buf in perf_buffers.iter_mut() {
@@ -272,9 +280,8 @@ pub async fn display_tcp_registry_events<T: BorrowMut<MapData>>(
                     for i in 0..events.read {
                         let data = &buffers[i];
                         if data.len() >= std::mem::size_of::<TcpPacketRegistry>() {
-                            let tcp_pl: TcpPacketRegistry = unsafe {
-                                std::ptr::read(data.as_ptr() as *const _)
-                            };
+                            let tcp_pl: TcpPacketRegistry =
+                                unsafe { std::ptr::read(data.as_ptr() as *const _) };
                             let src = reverse_be_addr(tcp_pl.src_ip);
                             let dst = reverse_be_addr(tcp_pl.dst_ip);
                             let src_port = u16::from_be(tcp_pl.src_port);
@@ -307,15 +314,13 @@ pub async fn display_tcp_registry_events<T: BorrowMut<MapData>>(
                                         dst,
                                         dst_port,
                                         command_str,
-                                        cgroup_id
-                                        //proc_content
+                                        cgroup_id //proc_content
                                     );
                                 }
                                 Err(_) => {
                                     info!(
                                         "Event Id: {} Protocol: Unknown ({})",
-                                        event_id,
-                                        tcp_pl.proto
+                                        event_id, tcp_pl.proto
                                     );
                                 }
                             };
@@ -353,7 +358,11 @@ pub async fn scan_cgroup_paths(path: String) -> Result<Vec<String>, Error> {
     let entries = match fs::read_dir(&target_path) {
         Ok(entries) => entries,
         Err(e) => {
-            error!("Error reading cgroup directory {:?}: {}", &target_path.clone(), e);
+            error!(
+                "Error reading cgroup directory {:?}: {}",
+                &target_path.clone(),
+                e
+            );
             return Ok(cgroup_paths);
         }
     };
@@ -375,9 +384,9 @@ pub async fn scan_cgroup_cronjob(time_delta: u64) -> Result<(), Error> {
     let interval = std::time::Duration::from_secs(time_delta);
     let mut discovered_pods = HashMap::<String, String>::new();
     while true {
-        let scanned_paths = scan_cgroup_paths(
-            "/sys/fs/cgroup/kubelet.slice".to_string()
-        ).await.expect("An error occured during the cgroup scan");
+        let scanned_paths = scan_cgroup_paths("/sys/fs/cgroup/kubelet.slice".to_string())
+            .await
+            .expect("An error occured during the cgroup scan");
         //--> this should return :
         //  /sys/fs/cgroup/kubelet.slice/kubelet-kubepods.slice
         //  /sys/fs/cgroup/kubelet.slice/kubelet.service
@@ -427,36 +436,33 @@ pub async fn scan_cgroup_cronjob(time_delta: u64) -> Result<(), Error> {
         }
 
         //read the subpaths
+        let mut uids = Vec::<String>::new();
         for subpath in scanned_subpaths_v2 {
-            //info!("Scanned cgroup subpath: {}", subpath);
-            let uids = extract_pod_uid(subpath.clone()).expect(
-                "An error occured during the extraction of pod UIDs"
-            );
-            info!("Debugging extracted UIDS: {:?}",&uids);
+            let uid = extract_pod_uid(subpath.clone())
+                .expect("An error occured during the extraction of pod UIDs");
+            debug!("Debugging extracted UID: {:?}", &uid);
+            uids.push(uid);
+        }
+        // get pod information from UID and store the info in an HashMqp for O(1) access
+        let service_map = get_pod_info().await?;
 
-            for uid in &uids {
-                // store the UID in an HashMap for O(1) access
-                discovered_pods.insert(uid.clone(), subpath.clone());
-                info!("Extracted Pod UID: {}", uid);
+        for (uid) in uids {
+            if let Some(name) = service_map.get(&uid) {
+                info!("UID (from eBPF): {} name:(from K8s): {}", &uid, name);
             }
-
-            // get pod information from UID
-            let service_map = get_pod_info_from_uid(&uids).await.expect("An error occured during the execution of the get_pod_info_from_uid function");
-
-            for (k,v) in service_map {
-                info!("Service name : {:?} , Service UID: {:?}",k,v);
-            }
-
         }
 
-        info!("Cronjob completed a cgroup scan cycle. Next scan will be in {} seconds", time_delta);
+        info!(
+            "Cronjob completed a cgroup scan cycle. Next scan will be in {} seconds",
+            time_delta
+        );
         time::sleep(interval).await;
     }
 
     Ok(())
 }
 
-fn extract_pod_uid(cgroup_path: String) -> Result<Vec<String>, Error> {
+fn extract_pod_uid(cgroup_path: String) -> Result<String, Error> {
     // example of cgroup path:
     // /sys/fs/cgroup/kubelet.slice/kubelet-kubepods.slice/kubelet-kubepods-besteffort.slice/kubelet-kubepods-besteffort-pod93580201_87d5_44e6_9779_f6153ca17637.slice
     // or
@@ -465,59 +471,107 @@ fn extract_pod_uid(cgroup_path: String) -> Result<Vec<String>, Error> {
     // split the path by "/"
     let splits: Vec<&str> = cgroup_path.split("/").collect();
     let mut uid_vec = Vec::<String>::new();
-    info!("Debugging splits: {:?}", &splits);
+    debug!("Debugging splits: {:?}", &splits);
 
-    if
-        splits[7].starts_with("kubelet-kubepods-besteffort-pod") ||
-        splits[7].starts_with("kubelet-kubepods-burstable-pod")
-    {
-        let uid = splits[7]
-            .replace("kubelet-kubepods-besteffort-pod", "")
-            .replace("kubelet-kubepods-burstable-pod", "")
-            .replace("kubelet-kubepods-pod", "")
-            .replace(".slice", "");
-        if !uid.is_empty() {
-            uid_vec.push(uid);
-        }
-    } else if
-        splits[7].starts_with("kubelet-kubepods-besteffort-pod")||
-        splits[7].starts_with("kubelet-kubepods-burstable-pod")
-    {
-        let uid = splits[7]
-            .replace("kubepods-besteffort-pod", "")
-            .replace("kubepods-burstable-pod", "")
-            .replace("kubepods-pod", "")
-            .replace(".slice", "");
+    let mut pod_split_vec = Vec::<String>::new();
 
-        if !uid.is_empty() {
-            uid_vec.push(uid);
-        }
-    }
+    let index = extract_target_from_splits(splits.clone())?;
 
-    Ok(uid_vec)
+    let pod_split = splits[index]
+        .trim_start_matches("kubelet-kubepods-besteffort-")
+        .trim_start_matches("kubelet-kubepods-burstable-")
+        .trim_start_matches("kubepods-besteffort-")
+        .trim_start_matches("kubepods-burstable-");
+
+    let uid_ = pod_split
+        .trim_start_matches("pod")
+        .trim_end_matches(".slice"); //return uids with underscore (_) [ex.dd3a1c6b_af40_41b1_8e1c_9e31fe8d96cb]
+
+    let uid = uid_.replace("_", "-");
+    Ok(uid.to_string())
 }
 
-async fn get_pod_info_from_uid(uids: &Vec<String>) -> Result<HashMap<String, String>, Error> {
-    // use the kube client to get the pod information from the UID
-    let client = Client::try_default().await.expect("Cannot connect to kubernetes client");
-    let pods: Api<Pod> = Api::all(client);
-
-    let mut service_map: HashMap<String, String> = HashMap::new();
-
-    for uid in uids {
-        let lp = kube::api::ListParams::default().fields(&format!("metadata.uid={}", uid));
-        info!("Debugging kube::api::ListParams: {:?}", &lp);
-        let pod_list = pods
-            .list(&lp).await
-            .expect("An error occured during the pod lits extraction");
-        for p in pod_list {
-            if let Some(name) = p.metadata.name {
-                service_map.insert(name, uid.to_string());
-            }
-            if let Some(namespace) = p.metadata.namespace {
-                info!("Pod Namespace: {} for UID: {}", namespace, uid);
-            }
+fn extract_target_from_splits(splits: Vec<&str>) -> Result<usize, Error> {
+    for (index, split) in splits.iter().enumerate() {
+        // find the split that contains the word 'pod'
+        if split.contains("-pod") {
+            debug!("Target index; {}", index);
+            return Ok(index);
         }
     }
+    Err(Error::msg("'-pod' word not found in split"))
+}
+
+/* unfortunately you cannot query the pods using the uids directly from ListParams */
+async fn query_all_pods() -> Result<ObjectList<Pod>, Error> {
+    let client = Client::try_default()
+        .await
+        .expect("Cannot connect to kubernetes client");
+    let pods: Api<Pod> = Api::all(client);
+    let lp = kube::api::ListParams::default(); // default list params
+    let pod_list = pods
+        .list(&lp)
+        .await
+        .expect("An error occured during the pod list extraction");
+
+    Ok(pod_list)
+}
+
+// fast pod caching system
+async fn get_pod_info() -> Result<HashMap<String, String>, Error> {
+    let all_pods = query_all_pods().await?;
+
+    let mut service_map = HashMap::<String, String>::new();
+
+    for pod in all_pods {
+        if let (Some(name), Some(uid)) = (pod.metadata.name, pod.metadata.uid) {
+            service_map.insert(uid, name);
+        }
+    }
+
     Ok(service_map)
+}
+
+mod tests {
+    use tracing_subscriber::fmt::format;
+
+    use crate::helpers::{extract_pod_uid, extract_target_from_splits};
+
+    #[test]
+    fn extract_uid_from_string() {
+        let cgroup_paths = vec!["/sys/fs/cgroup/kubepods.slice/kubepods-besteffort.slice/kubepods-besteffort-pod231bd2d7_0f09_4781_a4e1_e4ea026342dd.slice".to_string(),
+                                             "/sys/fs/cgroup/kubelet.slice/kubelet-kubepods.slice/kubelet-kubepods-besteffort.slice/kubelet-kubepods-besteffort-pod231bd2d7_0f09_4781_a4e1_e4ea026342dd.slice".to_string()];
+
+        let mut uid_vec = Vec::<String>::new();
+
+        for cgroup_path in cgroup_paths {
+            let uid = extract_pod_uid(cgroup_path)
+                .map_err(|e| format!("An error occured {}", e))
+                .unwrap();
+            uid_vec.push(uid);
+        }
+
+        let check = vec![
+            "231bd2d7-0f09-4781-a4e1-e4ea026342dd".to_string(),
+            "231bd2d7-0f09-4781-a4e1-e4ea026342dd".to_string(),
+        ];
+
+        assert_eq!(uid_vec, check);
+    }
+
+    #[test]
+    fn test_extract_target_index() {
+        let cgroup_paths = vec!["/sys/fs/cgroup/kubepods.slice/kubepods-besteffort.slice/kubepods-besteffort-pod231bd2d7_0f09_4781_a4e1_e4ea026342dd.slice".to_string(),
+                                             "/sys/fs/cgroup/kubelet.slice/kubelet-kubepods.slice/kubelet-kubepods-besteffort.slice/kubelet-kubepods-besteffort-pod231bd2d7_0f09_4781_a4e1_e4ea026342dd.slice".to_string()];
+
+        let mut index_vec = Vec::<usize>::new();
+        for cgroup_path in cgroup_paths {
+            let mut splits: Vec<&str> = cgroup_path.split("/").collect();
+
+            let target_index = extract_target_from_splits(splits).unwrap();
+            index_vec.push(target_index);
+        }
+        let index_check = vec![6, 7];
+        assert_eq!(index_vec, index_check);
+    }
 }
