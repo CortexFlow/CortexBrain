ae1ac3fd5f67e2b1c8839e7e94c6231f56d9551f
LorenzoTettamanti <lorenzo.tettamanti5@gmail.com>
2026-09-10 21:35:52 +0200

    (build): updated Dockerfile and build-conntracker script

--- core/src/components/conntracker/build-conntracker.sh
+++ core/src/components/conntracker/build-conntracker.sh
@@ -13,7 +13,9 @@ if ! command -v bindgen &> /dev/null; then
     export PATH="$HOME/.cargo/bin:$PATH" #add the ./cargo/bin directory to the PATH env variable
 fi
 
-bindgen vmlinux.h -o src/bindings.rs --use-core --allowlist-type 'sk_buff' --opaque-type 'ieee80211_sband_iftype_data'
+bindgen --use-core --no-layout-tests \
+    --allowlist-type 'sk_buff' \
+    vmlinux.h -o src/bindings.rs
 
 cargo +nightly build -Z build-std=core --target bpfel-unknown-none --release --bin conntracker
 
